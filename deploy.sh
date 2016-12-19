41e4c6d48e5796544b3106f6c3062740531c60ce
Fredrik Lindner <fredrik@epidemicsound.com>
2016-12-19 22:30:24 +0100

    rebuilding site Mon Dec 19 22:30:24 CET 2016

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,6 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-git subtree push --prefix=public git@github.com:fredlb/fredlb.github.io.git master 
 
 # Come Back
 cd ..
