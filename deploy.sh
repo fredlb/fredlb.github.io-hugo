9279ca74ad7248ae851215e9b0e1e9dd159b3fcb
Fredrik Lindner <fredrik@epidemicsound.com>
2016-12-19 22:16:21 +0100

    rebuilding site Mon Dec 19 22:16:21 CET 2016

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,6 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo --theme=hyde-y # if using a theme, replace by `hugo -t <yourtheme>`
 
-# Go To Public folder
-cd public
 # Add changes to git.
 git add -A
 
